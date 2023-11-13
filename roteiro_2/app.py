e43ad12118d37f589a03a7d9fc81d5c66d83d940
laupontiroli <laurapm@al.insper.edu.br>
2023-11-13 17:21:59 -0300

    aa

--- roteiro_2/app.py
+++ roteiro_2/app.py
@@ -3,15 +3,15 @@ import streamlit as st
 st.title("Roteiro 2: Widgets Interativos")
 
 value_slider = st.slider("Escolha um valor", 0, 100)
-value_selectbox = st.selectbox("Escolha seu gênero",("feminino","masculino","outro"),index=None,placeholder="Selecione seu gênero",)
+value_selectbox = st.selectbox("Escolha seu gênero",options=("feminino","masculino","outro"),index=None,placeholder="Selecione seu gênero",)
 value_checkbox = st. checkbox('Eu sou uma pessoa não um computador')
 value_radio = st.radio("Escolha sua escolaridade do momento",("fundamental","médio","superior",'não estou estudando'))
 value_toggle = st.toggle('Streamlit é legal')
 if st.button("Mostrar valores"):
     st.write(f'''
 Valor escolhido: {value_slider},
-gênero selecionado :{value_selectbox},
 é humano : {value_checkbox},
 escoliradade do momento : {value_radio},
 streamlit é legal : {value_toggle},
+gênero selecionado :
 ''',value_selectbox)
