ed71b3bf8479a7840ac6bd65be0c6074f94e7660
laupontiroli <laurapm@al.insper.edu.br>
2023-11-13 22:43:37 -0300

    sem api utils mais funcionando

--- roteiro_4/app.py
+++ roteiro_4/app.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import requests 
 
 st.title("Roteiro 4: Integração com APIs")
+
+pais = st.text_input("escreva o nome de um país")
+
+if pais:
+    try:
+        response = requests.get(f'https://restcountries.com/v3.1/name/{pais}')
+        response.raise_for_status()
+        message = response.json()
+        st.write(message)
+    except Exception as e : 
+        st.warning(e)
+
